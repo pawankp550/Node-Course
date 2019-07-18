b4798947f3c82a8a2facb0b5d761c48f8ff60c1f
pawankp550 <pawankp550@gmail.com>
2019-07-18 18:14:00 +0000

    Initial Commit

---
+++ Notes-app/app.js
@@ -0,0 +1,14 @@
+const getNotes = require('./notes');
+
+console.log(getNotes());
+
+
+
+
+
+
+
+// const fs = require('fs');
+
+// //fs.writeFileSync('test.txt', "File created by pawan");
+// fs.appendFileSync('test.txt',". File is appended by me again!!!!!")
