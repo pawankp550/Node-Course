d07d4edd451d589aa28e5fc8d42a3207d6f94d5a
pawankp550 <pawankp550@gmail.com>
2019-08-15 07:23:49 +0000

    to Deploy

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -1,26 +1,25 @@
 const request = require('request')
-const axios = require('axios')
-let latitude = 0
-let longitude = 0
-const url = `https://api.darksky.net/forecast/3f7d8cebe7f9dc6676e0cda89fad7676/37.8267,`
-const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/12what.json?access_token=pk.eyJ1IjoicGF3YW5rcDU1MCIsImEiOiJjanl5MG1mN2wwdnIyM2NtZXZueDF2N296In0.pX8uLRZxNRCOUbOqzfX-ZA&limit=1'
+const geoCode = require('./Utils/geoCode')
+const foreCast = require('./Utils/foreCast')
 
-request({ url, json: true }, (error, response) => {
-    if(error){
-         console.log('cannot connect to internet')
-     } else if(response.body.error){
-         console.log('invalid input')
-     } else {
-         console.log(response.body.currently)
-     }
-})
+const address = process.argv[2]
 
-request({url: geoUrl, json: true}, (error, response) => {
-    if(error){
-         console.log('cannot connect to internet')
-     } else if(response.body.features.length === 0){
-         console.log('Invalid Query')
-     } else{
-        console.log(response.body.features)
-     }
+if(!address){
+    return console.log('Please provide address')
+}
+
+geoCode(process.argv[2], (error, { cityName, latitude, longitude }) => {
+
+   if(error) {
+       return console.log(error)
+       }
+
+    foreCast(latitude, longitude, (error, foreCastData) => {
+        if(error){
+            return console.log(error)
+        }
+
+        console.log(cityName)
+        console.log(foreCastData)
+    })
 })
